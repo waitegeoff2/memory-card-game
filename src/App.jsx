10e06a4cd23ddae515bbe1eed3c344ffbee83320
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-06-05 09:33:59 -0400

    Added useeffect to fetch from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [cards, setCards] = useState([]);
+
+  //STATE FOR CARDS set to null (update when you fetch them)
+  //STEP ONE, DO THIS FIRST TMRW, FILL THE CARD ARRAY WITH STUFF YOU FETCH BELOW, pass it into the display card function as a prop
+
+  //function to fetch the data from API
+  //fetch 10 photos and give them ids
+  //this will run when component first renders, fetch some data
+  useEffect(
+    () => {
+      fetch('https://api.artic.edu/api/v1/artworks')
+        .then(response => {
+          return response.json();
+        })
+        .then((data) => {
+          setCards(data.data)
+          
+        })
+      console.log("use effect ran");
+    }, []
+  );
+
+  console.log(cards)
+
   return (
     <>
-     <div>hi</div>
+     <div>
+      
+     </div>
     </>
   )
 }
--- src/components/displayCards.jsx
+++ src/components/displayCards.jsx
@@ -1,2 +1,33 @@
 import '../styles/displayCardsStyles.css'
 
+//to store the cards the person has already chosen
+//map it to prevent duplicates
+//length it to count current score
+let chosenCards = []; //STATE
+let cardOrder = [] //STATE (this is where you put the cards and then put it into the array)
+
+
+//https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg
+//identifier is thing.image_id
+
+function handleClick () {
+    //when someone clicks a card, take that card and put it into an array of chosen card (useState?)
+    //update score
+    //run randomizecard again
+}
+
+function cardArray() {
+    //function to build cardarray
+}
+
+export default function displayRandomCards(propcards) {
+    //TWO STEPS: randomize the array and display it (each on has an onclick)
+
+    //take array of cards
+    //randomize order and SETSTATE in array
+        //const card1 = cardorder[1]
+            //array.map(item) => return <div>{item}<div> (MAYBE YOU JUST NEED THIS, you're just mapping the new array each time)
+        //etc.
+    //return divs with random order of cards. onclick {function }
+}
+
