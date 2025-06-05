b09ae061443c87142054d451d3ca244f2da0d247
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-06-05 12:05:15 -0400

    Reorganizing displayCards component

--- src/components/displayCards.jsx
+++ src/components/displayCards.jsx
@@ -9,54 +9,47 @@ import '../styles/displayCardsStyles.css'
 let cardOrder = [] //STATE (this is where you put the cards and then put it into the array)
 
 
+
 //https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg
 //identifier is thing.image_id
 
-function handleClick(id) {
-    console.log(id);
-    //if id matches something already in array
-    //end game
-
-    if(cardOrder.includes(id)) {
-        console.log("game failed")
-    } else {
-        cardOrder.push(id)
-    }
-    
-    console.log(cardOrder)
-    
-    //when someone clicks a card, take that card and put it into an array of chosen card (useState?) WITH AN INDEX
-    //update score
-    //run randomizecard again
-}
-
-function cardArray() {
-    //function to build cardarray
-}
-
-function randomizeArr(array) {
-
-    let newArray = [...array];
-    let currentIndex = newArray.length;
-
-    while (currentIndex != 0) {
-
-    // Pick a remaining element
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [newArray[currentIndex], newArray[randomIndex]] = [
-      newArray[randomIndex], newArray[currentIndex]];
-  }
-  return newArray;
-}
-
 export default function DisplayRandomCards({cards}) {
     const [chosenCards, setChosenCards] = useState([]);
+
+    //MOVE THIS SOMEWHERE? so it can run this component again????
+    function handleClick(id) {
+        console.log(id);
+        //if id matches something already in array
+        //end game
+
+        if(cardOrder.includes(id)) {
+            console.log("game failed")
+        } else {
+            cardOrder.push(id)
+        }
+        
+        console.log(cardOrder)
+    }
+
+    function randomizeArr(array) {
+        let newArray = [...array];
+        let currentIndex = newArray.length;
+
+        while (currentIndex != 0) {
+            // Pick a remaining element
+            let randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [newArray[currentIndex], newArray[randomIndex]] = [
+            newArray[randomIndex], newArray[currentIndex]];
+        }
+
+        return newArray;
+    }
+
     //TWO STEPS: randomize the array and display it (each on has an onclick)
     //randomize array
-    console.log(cards)
 
     let newArray = randomizeArr(cards);
     console.log(newArray)
