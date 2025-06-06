e0e660ecd89a7cbc018d2b0074b594275b00792e
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-06-06 09:10:26 -0400

    SetCards to hold 10 pictures from top level of app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import DisplayRandomCards from './components/displayCards';
 
 function App() {
 
-  const [cards, setCards] = useState();
+  const [cards, setCards] = useState([]);
   const [score, setScore] = useState(0);
 
   //SET THE CARDS ARRAY HERE AND PASS IT IN. DO IT WITH A NEW API AND JUST GET 10 CARD OBJECTS
@@ -15,17 +15,26 @@ function App() {
   //this will run when component first renders, fetch some data
   useEffect(
     () => {
-      fetch('https://picsum.photos/v2/list?page=2&limit=10')
+      fetch('https://picsum.photos/v2/list?page=2&limit=10') //you want PAUL JARVIS photos, hipster website
+      ///200 for SQUARE PHOTOS
         .then(response => {
           return response.json();
         })
         .then((data) => {
           // setCards(data.data)
           console.log(data)
-          //put 10 of these into CARDS -> SETCARDS
+          console.log(data[0])
+
+          setCards([data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9]])
+
+          //let newCards.....
+          //SETCARDS(...cards, [data[0], data[1], data[2] ALL 10]) putting these 10 cards in an array
         })
     }, []
   );
+
+
+  console.log(cards)
 
   return (
     <>
